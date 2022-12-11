ff50b0a4c55caf615ff2316c81feeea175f1076c
KarinaSol <karinasolmeza83@gmail.com>
2022-12-11 18:33:03 -0300

    Proveedores sin relacionar crud funcionando

---
+++ ProyectoFinal/Controllers/ProveedorController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Datos;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class ProveedorController : Controller
+    {
+       
+            ProveedorDatos proveedorDatos = new ProveedorDatos();
+
+
+
+            public IActionResult Index()
+            {
+
+                var oLista = proveedorDatos.ListarProveedor();
+
+                return View(oLista);
+            }
+
+            public IActionResult GuardarProveedor()
+            {
+                return View();
+            }
+
+
+            [HttpPost]
+            public IActionResult GuardarProveedor(Proveedor oProveedor)
+            {
+                var respuesta = proveedorDatos.GuardarProveedor(oProveedor);
+
+                if (respuesta)
+                {
+
+                    return RedirectToAction("Index");
+
+                }
+
+                else
+                {
+                    return View();
+                }
+            }
+
+
+            //Método para la vista
+            public IActionResult EditarProveedor(int id)
+            {
+
+                var oProveedor = proveedorDatos.ObtenerProveedor(id);
+
+                return View(oProveedor);
+            }
+
+
+
+
+
+            [HttpPost]
+            public IActionResult EditarProveedor(Proveedor oProveedor)
+            {
+                var respuesta = proveedorDatos.EditarProveedor(oProveedor);
+
+                if (respuesta)
+                {
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+
+
+            //Método para vista eliminar 
+
+            public IActionResult EliminarProveedor(int id)
+            {
+                var oProveedor = proveedorDatos.ObtenerProveedor(id);
+
+                return View(oProveedor);
+            }
+
+            //Método para logica de eliminar 
+            [HttpPost]
+            public IActionResult EliminarProveedor(Proveedor oProveedor)
+            {
+                var respuesta = proveedorDatos.EliminarProveedor(oProveedor.id_proveedor);
+
+                if (respuesta)
+                {
+
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    return View();
+                }
+            }
+
+        }
+    }
---
+++ ProyectoFinal/Models/Proveedor.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoFinal.Models
+{
+    public class Proveedor
+    {
+        public int id_proveedor { get; set; }
+
+        public string? prove_nombre { get; set; }
+
+        public string? prove_apellido { get; set; }
+
+        public string? prove_direccion { get; set; }
+
+        public string? prove_cuit { get; set; }
+
+      
+
+    }
+}
