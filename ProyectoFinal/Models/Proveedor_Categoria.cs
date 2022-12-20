ccf21ca335faa9e74fd93c3b7e7d1fd2e2c9965b
KarinaSol <karinasolmeza83@gmail.com>
2022-12-20 16:41:09 -0300

    Relacion m-m Proveedor Categoria

---
+++ ProyectoFinal/Models/Proveedor_Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinal.Models
+{
+    public class Proveedor_Categoria
+    {
+
+       
+        [ForeignKey("Proveedor")]
+        public int? id_proveedor { get; set; }
+
+        [ForeignKey("Categoria")]
+        public int? id_categoria { get; set; }
+
+
+        public Proveedor proveedorCategoria { get; set; }
+
+        public Categoria categoriaProveedor { get; set; }
+
+
+    }
+}
